52a98c73c16e17c31c3b5f9d359fab57c1a61dd0
Kate2610 <yekaterina.anisimova@gmail.com>
2023-03-31 14:27:50 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Searchbar from './components/Searchbar';
 import ImageGallery from './components/ImageGallery';
@@ -10,116 +10,96 @@ import { ReactComponent as CloseIcon } from './images/close.svg';
 
 import fetchImages from './api/api-services';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      images: [],
-      currentPage: 1,
-      searchQuery: '',
-      isLoading: false,
-      showModal: false,
-      largeImage: '',
-      error: null,
-    };
-  }
+const App = () => {
+const [images, setImages] = useState([]);
+const [currentPage, setCurrentPage] = useState(1);
+const [searchQuery, setSearchQuery] = useState('');
+const [isLoading, setIsLoading] = useState(false);
+const [showModal, setShowModal] = useState(false);
+const [largeImage, setLargeImage] = useState('');
+const [error, setError] = useState(null);
 
-  componentDidMount() {
-    const { searchQuery } = this.state;
-    if (searchQuery) {
-      this.getImages();
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const { searchQuery } = this.state;
-    if (prevState.searchQuery !== searchQuery) {
-      this.getImages();
-    }
-  }
-
-  onChangeQuery = query => {
-    this.setState({
-      images: [],
-      currentPage: 1,
-      searchQuery: query,
-      isLoading: false,
-      showModal: false,
-      largeImage: '',
-      error: null,
-    });
-  };
-
-  getImages = async () => {
-    const { currentPage, searchQuery } = this.state;
-    this.setState({ isLoading: true });
-
-    try {
-      const { hits } = await fetchImages(searchQuery, currentPage);
-
-      this.setState(prevState => ({
-        images: [...prevState.images, ...hits],
-        currentPage: prevState.currentPage + 1,
-      }));
-
-      if (currentPage !== 1) {
-        this.scrollOnLoadButton();
-      }
-    } catch (error) {
-      console.log('Smth wrong with App fetch', error);
-      this.setState({ error });
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  };
-
-  handleGalleryItem = fullImageUrl => {
-    this.setState({ largeImage: fullImageUrl, showModal: true });
-  };
-
-  toggleModal = () => {
-    this.setState(prevState => ({ showModal: !prevState.showModal }));
-  };
-
-  scrollOnLoadButton = () => {
-    window.scrollTo({
-      top: document.documentElement.scrollHeight,
-      behavior: 'smooth',
-    });
-  };
-
-  needToShowLoadMore = () => {
-    const { images } = this.state;
-    return images.length > 0 && images.length >= 12;
-  };
-
-  render() {
-    const { images, isLoading, showModal, largeImage, error } = this.state;
-
-    return (
-      <>
-        <Searchbar onSearch={this.onChangeQuery} />
-        <ImageGallery images={images} onImageClick={this.handleGalleryItem} />
-
-        {this.needToShowLoadMore() && <Button onClick={this.getImages} />}
-
-        {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <div className="Close-box">
-              <IconButton onClick={this.toggleModal} aria-label="Close modal">
-                <CloseIcon width="20px" height="20px" fill="#7e7b7b" />
-              </IconButton>
-            </div>
-            <img src={largeImage} alt="" className="Modal-image" />
-          </Modal>
-        )}
-
-        {isLoading && <Loader />}
-
-        {error}
-      </>
-    );
-  }
+useEffect(() => {
+if (searchQuery) {
+getImages();
 }
+}, [searchQuery]);
+
+useEffect(() => {
+if (currentPage !== 1) {
+scrollOnLoadButton();
+}
+}, [images]);
+
+const onChangeQuery = query => {
+setImages([]);
+setCurrentPage(1);
+setSearchQuery(query);
+setIsLoading(false);
+setShowModal(false);
+setLargeImage('');
+setError(null);
+};
+
+const getImages = async () => {
+  setIsLoading(true);
+
+  try {
+  const { hits } = await fetchImages(searchQuery, currentPage);
+  setImages(prevImages => [...prevImages, ...hits]);
+  setCurrentPage(prevPage => prevPage + 1);
+  }
+  catch (error) {
+    console.log('Smth wrong with App fetch', error);
+    setError(error);
+  }
+  finally {
+    setIsLoading(false);
+    }
+  };
+
+const handleGalleryItem = fullImageUrl => {
+    setLargeImage(fullImageUrl);
+    setShowModal(true);
+};
+
+const toggleModal = () => {
+   setShowModal(prevModal => !prevModal);
+};
+
+const scrollOnLoadButton = () => {
+   window.scrollTo({
+   top: document.documentElement.scrollHeight,
+   behavior: 'smooth',
+});
+};
+
+const needToShowLoadMore = () => {
+   return images.length > 0 && images.length >= 12;
+};
+
+return (
+<>
+<Searchbar onSearch={onChangeQuery} />
+    <ImageGallery images={images} onImageClick={handleGalleryItem} />
+  {needToShowLoadMore() && <Button onClick={getImages} />}
+
+  {showModal && (
+    <Modal onClose={toggleModal}>
+      <div className="Close-box">
+        <IconButton onClick={toggleModal} aria-label="Close modal">
+          <CloseIcon width="20px" height="20px" fill="#7e7b7b" />
+        </IconButton>
+      </div>
+      <img src={largeImage} alt="" className="Modal-image" />
+    </Modal>
+  )}
+
+  {isLoading && <Loader />}
+
+  {error}
+</>
+);
+};
 
 export default App;
